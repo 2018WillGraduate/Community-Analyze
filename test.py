f1901dd55de15106ed1de76261955816e2a0105d
15479 <823125@naver.com>
2023-06-27 21:28:11 +0900

    make folder

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 from dcinside.crawler import Crawler
 import json
+import os
+
+def create_directory(path):
+    try:
+        os.makedirs(path)
+        print(f"Directory '{path}' created.")
+    except FileExistsError:
+        print(f"Directory '{path}' already exists.")
+
+gallery = "tree"
+
+create_directory(gallery)
+
 
 crawler = Crawler()
-for i in range(1, 101):
+for i in range(1,101):
     try:
-        res = crawler.crawl("kartriderdrift", str(i))
-        with open(f"data/kartriderdrift_{i}.json", "w", encoding="utf-8") as f:
+        res = crawler.crawl(gallery, str(i))
+        with open(f"./{gallery}/{gallery}_{i}.json", "w", encoding="utf-8") as f:
             f.write(json.dumps(res, ensure_ascii=False, indent=4))
     except Exception as e:
         print(e)
